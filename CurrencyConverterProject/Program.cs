691457ed7b8d3367ac2e62f387b8ce1b653da4f4
lekeeeeee <rozhajaleka@gmail.com>
2023-11-29 15:17:16 +0100

    CurrencyChanges

--- CurrencyConverterProject/Program.cs
+++ CurrencyConverterProject/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-// Program.cs
 namespace CurrencyConverter
 {
     class Program
@@ -13,10 +6,32 @@ namespace CurrencyConverter
         static void Main(string[] args)
         {
             var service = new ExternalCurrencyService();
-            ICurrencyConverter converter = new CurrencyServiceAdapter(service);
+            var converter = new CurrencyServiceAdapter(service);
 
-            double amount = converter.Convert("USD", "EUR", 50);
-            Console.WriteLine($"Converted Amount: {amount}");
+            // Prompt the user to enter the amount to be converted
+            Console.WriteLine("Please enter the amount in USD you want to convert to EUR:");
+            string input = Console.ReadLine();
+
+            try
+            {
+                // Attempt to parse the input to a double
+                double amountToConvert = double.Parse(input);
+
+                // Perform the conversion
+                double convertedAmount = converter.Convert("USD", "EUR", amountToConvert);
+
+                // Output the result
+                Console.WriteLine($"{amountToConvert} USD is {convertedAmount} EUR.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The amount entered is not a valid number.");
+            }
+            catch (Exception ex)
+            {
+                // Handle any other unexpected exceptions
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
     }
 }
